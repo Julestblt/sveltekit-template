4299518a5c9d066429658beea50bb2f5162dc07f
Julestblt <jules.thibault2002@gmail.com>
2022-01-10 11:42:18 +0100

    🔧 add cypress configuration

---
+++ cypress/plugins/index.ts
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+module.exports = (on) => {
+	// @bahmutov/cy-rollup is already a dependency of cypress-svelte-unit-test
+	const filePreprocessor = require('@bahmutov/cy-rollup');
+	on('file:preprocessor', filePreprocessor());
+};
